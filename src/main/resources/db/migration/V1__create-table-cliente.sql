3df9d2a4d98b74956223ced29cb3d09114bee5fd
Itamar <itamarpj00@hotmail.com>
2025-05-13 17:08:49 -0300

    [fix]: mudança no tipo de ID na migration da tabela Cliente

--- src/main/resources/db/migration/V1__create-table-cliente.sql
+++ src/main/resources/db/migration/V1__create-table-cliente.sql
@@ -1,5 +1,5 @@
 CREATE TABLE cliente (
-    id BIGINT PRIMARY KEY,
+    id BIGSERIAL PRIMARY KEY NOT NULL,
     nome VARCHAR(255) NOT NULL,
     cnpj VARCHAR(14) NOT NULL UNIQUE,
     atividade_economica VARCHAR(255),
